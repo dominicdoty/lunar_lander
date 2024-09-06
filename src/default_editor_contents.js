176cf9bce5caf71e28ea935e4a676c1be623e69a
Dominic Doty <doty.dominic@gmail.com>
2024-09-06 14:25:46 -0600

    Expose simulation parameters to the user via a comment in the default editor contents per feedback in #2

--- src/default_editor_contents.js
+++ src/default_editor_contents.js
@@ -1,3 +1,31 @@
+/*
+ * Notes on simulation parameters:
+ * Generally simulation parameters are not selected for realism
+ * but instead to try to make the game fun.
+ *
+ * Angular units are presented in degrees, while linear units are pixels.
+ *
+ * If "Fuel Mass" is enabled in the Options tab, fuel mass will
+ * affect acceleration of the spacecraft. If not, the initial fuel
+ * mass is used for the entire landing.
+ *
+ * Spacecraft Mass:
+ *   initial fuel_mass = 10
+ *   mass = 10 + fuel_mass
+ *   fuel consumption = (|rotThrust| * 0.02) + (|aftThrust| * 0.05)
+ *   you must track fuel consumed
+ *
+ * Acceleration:
+ *   gravity = 1 / 60
+ *   linear = aftThrust / mass  (in direction of current lander angle)
+ *   angular = rotThrust / mass (used as an approximation of MOI)
+ *
+ * Successful landing parameters:
+ *   |angle|       < 10  [degrees] from vertical
+ *   |linear  vel| < 1   [units/second]
+ *   |angular vel| < 0.5 [degrees/second]
+ */
+
 // Arguments:
 let {
     x_position,
@@ -18,7 +46,7 @@ log("x_pos: ", x_position.toFixed(1));
 
 // Example of how to plot to the plots tab
 plot({
-    altitude: altitude
+    altitude: altitude,
 });
 
 // Return:
