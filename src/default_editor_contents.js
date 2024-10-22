2c0e68caf4aad06b115270971fca7f5b19764b8c
Dominic Doty <doty.dominic@gmail.com>
2024-10-22 11:39:32 -0600

    Update editor default contents with new tab size default autoformat

--- src/default_editor_contents.js
+++ src/default_editor_contents.js
@@ -28,17 +28,17 @@
 
 // Arguments:
 let {
-    x_position,
-    altitude,
-    angle,
-    userStore,
-    log,
-    plot
+  x_position,
+  altitude,
+  angle,
+  userStore,
+  log,
+  plot
 } = arguments[0];
 
 // Example of how to initialize user storage
 if (!("store" in userStore)) {
-    userStore.store = 0;
+  userStore.store = 0;
 }
 
 // Example of how to log to the user accessible console
@@ -46,12 +46,12 @@ log("x_pos: ", x_position.toFixed(1));
 
 // Example of how to plot to the plots tab
 plot({
-    altitude: altitude,
+  altitude: altitude,
 });
 
 // Return:
 return {
-    rotThrust: 0,
-    aftThrust: 0,
-    userStore: userStore,
+  rotThrust: 0,
+  aftThrust: 0,
+  userStore: userStore,
 };
